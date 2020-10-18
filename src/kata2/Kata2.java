e2029a78f48510dadbf999924ac3c746d044f60e
Yar <Yar@DESKTOP-Yaret>
2020-10-18 19:11:51 +0100

    kata2 versión 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -11,6 +11,10 @@ public class Kata2 {
         Integer[] data = {1,8,5,4,9,7,88,8,5,4,22,6,8,2,4};
         Map<Integer,Integer> histogram = new HashMap<Integer,Integer>();
         
+        for (Integer key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
+        /*
         for (int i = 0; i < data.length; i++) {
             if(histogram.containsKey(data[i])){
                 histogram.put(data[i], histogram.get(data[i])+1);
@@ -19,6 +23,7 @@ public class Kata2 {
             }
             
         }
+        */
         
         Iterator<Map.Entry<Integer,Integer>> entries = histogram.entrySet().iterator();
         while (entries.hasNext()) {
